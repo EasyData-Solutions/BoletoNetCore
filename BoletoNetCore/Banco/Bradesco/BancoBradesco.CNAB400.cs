66c90da29e408e56927eea9ad1d76558d84e2767
David Freire <davidcarlosfreire@hotmail.com>
2023-09-07 11:56:37 -0300

    20230907

--- BoletoNetCore/Banco/Bradesco/BancoBradesco.CNAB400.cs
+++ BoletoNetCore/Banco/Bradesco/BancoBradesco.CNAB400.cs
@@ -20,6 +20,22 @@ namespace BoletoNetCore
             return detalhe;
         }
 
+        public override void CompletarHeaderRetornoCNAB400(string registro)
+        {
+            this.Beneficiario.ContaBancaria = new ContaBancaria();
+            this.Beneficiario.ContaBancaria.Agencia = registro.Substring(25, this.TamanhoAgencia);
+
+            var conta = registro.Substring(30, 7).Trim();
+            this.Beneficiario.ContaBancaria.Conta = conta.Substring(0, conta.Length - 1);
+            this.Beneficiario.ContaBancaria.DigitoConta = conta.Substring(conta.Length - 1, 1);
+
+            // 01 - cpf / 02 - cnpj
+            if (registro.Substring(1, 2) == "01")
+                this.Beneficiario.CPFCNPJ = registro.Substring(6, 11);
+            else
+                this.Beneficiario.CPFCNPJ = registro.Substring(3, 14);
+        }
+
         public void LerDetalheRetornoCNAB400Segmento1(ref Boleto boleto, string registro)
         {
             try
