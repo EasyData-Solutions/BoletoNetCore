97a79e45d0cf859791393ea41b7b8fabca54c347
David Freire <davidcarlosfreire@hotmail.com>
2026-07-27 10:02:17 -0300

    Correção campo de CNPJ/CPF para aceitar CNPJ Alfanumerico

--- BoletoNetCore/Boleto/Pagador.cs
+++ BoletoNetCore/Boleto/Pagador.cs
@@ -24,7 +24,7 @@ namespace BoletoNetCore
             }
             set
             {
-                string numero = value.Trim().Replace(".", "").Replace("-", "").Replace("/", "");
+                string numero = value.Replace(".", "").Replace("-", "").Replace("/", "");
                 if (numero == null || (numero.Length != 11 && numero.Length != 14))
                     throw new ArgumentException("CPF/CNPJ inválido: Utilize 11 dígitos para CPF ou 14 para CNPJ.");
                 _cpfcnpj = numero;
